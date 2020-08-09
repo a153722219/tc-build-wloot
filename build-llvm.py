017e87d8912e0c15503fb2eb079df54d84e337e7
Julian Liu <wlootlxt123@gmail.com>
2020-08-09 17:42:02 +0800

    Revert "build-llvm.py: Try to use ar and ranlib on stage 1"
    This reverts commit 10e53467d1c4450d7c2e0ae5e8195ba19d0f9c3a.

--- build-llvm.py
+++ build-llvm.py
@@ -29,12 +29,10 @@ class Directories:
 
 
 class EnvVars:
-    def __init__(self, ar, cc, cxx, ld, ranlib):
-        self.ar = ar
+    def __init__(self, cc, cxx, ld):
         self.cc = cc
         self.cxx = cxx
         self.ld = ld
-        self.ranlib = ranlib
 
 
 def clang_version(cc, root_folder):
@@ -429,10 +427,7 @@ def check_cc_ld_variables(root_folder):
             ld_to_print = shutil.which(ld)
         print("LD: " + ld_to_print)
 
-    ar = shutil.which("llvm-ar")
-    ranlib = shutil.which("llvm-ranlib")
-
-    return ar, cc, cxx, ld, ranlib
+    return cc, cxx, ld
 
 
 def check_dependencies():
@@ -687,13 +682,13 @@ def cc_ld_cmake_defines(dirs, env_vars, stage):
     defines = {}
 
     if stage == 1:
-        ar = env_vars.ar
+        ar = None
         cc = env_vars.cc
         clang_tblgen = None
         cxx = env_vars.cxx
         ld = env_vars.ld
         llvm_tblgen = None
-        ranlib = env_vars.ranlib
+        ranlib = None
     else:
         ar = get_stage1_binary("llvm-ar", dirs)
         cc = get_stage1_binary("clang", dirs)
@@ -703,6 +698,8 @@ def cc_ld_cmake_defines(dirs, env_vars, stage):
         llvm_tblgen = get_stage1_binary("llvm-tblgen", dirs)
         ranlib = get_stage1_binary("llvm-ranlib", dirs)
 
+    # Use llvm-ar for stage 2+ builds to avoid errors with bfd plugin
+    # bfd plugin: LLVM gold plugin has failed to create LTO module: Unknown attribute kind (60) (Producer: 'LLVM9.0.0svn' Reader: 'LLVM 8.0.0')
     if ar:
         defines['CMAKE_AR'] = ar
 
@@ -722,6 +719,7 @@ def cc_ld_cmake_defines(dirs, env_vars, stage):
     if llvm_tblgen:
         defines['LLVM_TABLEGEN'] = llvm_tblgen
 
+    # Use llvm-ranlib for stage 2+ builds
     if ranlib:
         defines['CMAKE_RANLIB'] = ranlib
 
